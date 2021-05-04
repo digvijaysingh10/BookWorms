e3a5e6e24d0ab45fd681a08ea9aede9dde9df5f3
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2021-05-04 12:11:40 +0530

    lord

--- src/app/user/manage-novels/manage-novels.component.ts
+++ src/app/user/manage-novels/manage-novels.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NovelService } from 'src/app/services/novel.service';
+import { app_config } from 'src/config';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,6 +10,7 @@ import Swal from 'sweetalert2';
   styleUrls: ['./manage-novels.component.css'],
 })
 export class ManageNovelsComponent implements OnInit {
+  url = app_config.api_url+'/';
   novelsList: any;
   rentNovels: any;
   exchangeNovels: any;
