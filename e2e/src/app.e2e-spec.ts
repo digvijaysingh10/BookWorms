5eac71a1672409e15f0e1b22edd3ce70d9de63c4
DIGVIJAY <singhdigvijay703@gmail.com>
2021-05-17 19:37:07 +0530

    masterTemplete to bookWorms

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('masterTemplate app is running!');
+    expect(page.getTitleText()).toEqual('bookWorms app is running!');
   });
 
   afterEach(async () => {
