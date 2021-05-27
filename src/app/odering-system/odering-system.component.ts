29d71605361a60fa4ddd4defd98d13b9f6161f92
DIGVIJAY <singhdigvijay703@gmail.com>
2021-05-27 11:37:32 +0530

    responsive

---
+++ src/app/odering-system/odering-system.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-odering-system',
+  templateUrl: './odering-system.component.html',
+  styleUrls: ['./odering-system.component.css']
+})
+export class OderingSystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
