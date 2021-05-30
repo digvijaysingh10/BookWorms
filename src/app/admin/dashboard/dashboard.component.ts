ced398e3d42df6109940ff1e906c636730d493b5
DIGVIJAY <singhdigvijay703@gmail.com>
2021-05-30 22:04:29 +0530

    fetct user and novel done

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NovelService } from 'src/app/services/novel.service';
+import { OrderService } from 'src/app/services/order.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  usersList: any;
+  loadingUsers = true;
+  novelsList: any;
+  rentNovels: any;
+  exchangeNovels: any;
+  sellNovels: any;
+  loadingNovels = true;
+
+  constructor(
+    public userService: UserService,
+    public novelService: NovelService,
+    public orderService: OrderService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-
+    this.fetchUsers();
+    this.fetchNovels();
   }
+
+  fetchUsers() {
+    this.userService.getAll().subscribe((res) => {
+      this.usersList = res;
+      this.loadingUsers = false;
+    });
+  }
+
+  filterNovels() {
+    this.rentNovels = this.novelsList.filter((novel) => novel.rentable);
+    console.log(this.rentNovels);
+    this.exchangeNovels = this.novelsList.filter((novel) => novel.exchangable);
+    console.log(this.exchangeNovels);
+    this.sellNovels = this.novelsList.filter((novel) => novel.soldable);
+    console.log(this.sellNovels);
+  }
+
+  fetchNovels() {
+    this.novelService
+      .getNovelByUser(this.userService.currentUser._id)
+      .subscribe((res) => {
+        this.novelsList = res;
+        this.loadingNovels = false;
+        console.log(this.novelsList);
+        this.filterNovels();
+      });
+  }
+
 
 }
