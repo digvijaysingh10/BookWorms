eecb66687705ca04309f7446557fff9eb62282e0
DIGVIJAY <singhdigvijay703@gmail.com>
2021-05-30 21:55:21 +0530

    ..

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { app_config } from 'src/config';
 
 @Injectable({
@@ -7,5 +9,9 @@ import { app_config } from 'src/config';
 export class OrderService {
 
   url = app_config.api_url + '/order';
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getAll() {
+    return this.http.get(this.url + '/getall');
+  }
 }
