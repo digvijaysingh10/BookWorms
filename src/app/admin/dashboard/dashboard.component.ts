f3d0437e624a850fe0eeec49eb8b07896994ac7a
DIGVIJAY <singhdigvijay703@gmail.com>
2021-05-31 21:46:38 +0530

    ..

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { NovelService } from 'src/app/services/novel.service';
 import { OrderService } from 'src/app/services/order.service';
 import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -58,5 +60,102 @@ export class DashboardComponent implements OnInit {
       });
   }
 
+  drawchart(id, datapoints, title, unit, xlabel) {
+    var chart = new CanvasJS.Chart(id, {
+      animationEnabled: true,
+      theme: 'light2',
+      title: {
+        text: title,
+      },
+      axisY: {
+        title: xlabel,
+        titleFontSize: 24,
+      },
+      data: [
+        {
+          type: 'column',
+          yValueFormatString: `#,### ${unit}`,
+          dataPoints: datapoints,
+        },
+      ],
+    });
+    chart.render();
+  }
+
+  prepareRegData(users) {
+    this.getDatewiseValues(users, 'created').subscribe((data) => {
+      console.log(data);
+      let regData = data;
+      this.drawchart(
+        'regByDate',
+        regData,
+        'User Registration Data',
+        'registration(s)',
+        'No. of Registrations'
+      );
+    });
+  }
+
+  prepareComData(companies) {
+    this.getDatewiseValues(companies, 'created').subscribe((data) => {
+      console.log(data);
+      let regData = data;
+      this.drawchart(
+        'companyByDate',
+        regData,
+        'Company Registration Data',
+        'registration(s)',
+        'No. of Registrations'
+      );
+    });
+  }
+
+  getDatewiseValues(records, colname) {
+    console.log(records);
+    return Observable.create((observer) => {
+      let datewise = [];
+      this.getUniqueValues(colname, records).subscribe((unique_values) => {
+        for (let value of unique_values[1]) {
+          console.log(value);
+          datewise.push({
+            x: new Date(value),
+            y: this.getCount(unique_values[0], value),
+          });
+        }
+        observer.next(datewise);
+      });
+    });
+  }
+
+  getUniqueValues(col_name, data) {
+    // console.log(col_name+' '+data);
+    return Observable.create((observer) => {
+      let values = data.map((ele) => {
+        let date = new Date(ele[col_name]).setHours(0, 0, 0, 0);
+        // console.log(new Date(date).getTime());
+        return new Date(date).getTime();
+      });
+
+      let uniquevalues = [];
+      for (let value of values) {
+        if (!uniquevalues.includes(value)) {
+          uniquevalues.push(value);
+          // console.log(value);
+        }
+      }
+      observer.next([values, uniquevalues]);
+    });
+  }
+
+  getCount(records, item) {
+    let count = 0;
+    for (let record of records) {
+      if (record == item) {
+        count++;
+      }
+    }
+
+    return count;
+  }
 
 }
