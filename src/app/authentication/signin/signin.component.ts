908aee0321bc027e3ec2aa0b90736ab6a29d13fa
DIGVIJAY <singhdigvijay703@gmail.com>
2021-06-04 20:44:11 +0530

    validation in signup

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import {
   GoogleLoginProvider,
@@ -174,4 +174,28 @@ export class SigninComponent implements OnInit {
   toggleSignIn() {
     document.getElementById('container').classList.remove('right-panel-active');
   }
+
+  // MustMatch(controlName: string, matchingControlName: string) {
+  //   return (formGroup: FormGroup) => {
+  //       const control = formGroup.controls[controlName];
+  //       const matchingControl = formGroup.controls[matchingControlName];
+
+  //       // return null if controls haven't initialised yet
+  //       if (!control || !matchingControl) {
+  //         return null;
+  //       }
+
+  //       // return null if another validator has already found an error on the matchingControl
+  //       if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+  //           return null;
+  //       }
+
+  //       // set error on matchingControl if validation fails
+  //       if (control.value !== matchingControl.value) {
+  //           matchingControl.setErrors({ mustMatch: true });
+  //       } else {
+  //           matchingControl.setErrors(null);
+  //       }
+  //   }
+  // }
 }
