018cba88af373829e994cef8aebeecd001a2595a
m-triple-m <triplem656@gmail.com>
2021-06-06 14:46:00 +0530

    ..

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,23 +5,20 @@ import { RequestService } from '../services/request.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   requestList;
   url = app_config.api_url + '/';
-  constructor(
-    private requestService: RequestService
-    ) { }
+  constructor(private requestService: RequestService) {}
 
   ngOnInit(): void {
     this.fetchRequests();
   }
 
   fetchRequests() {
-    this.requestService.getAll().subscribe((data:any) => {
-      this.requestList = data.slice(0,6);
+    this.requestService.getAll().subscribe((data: any) => {
+      this.requestList = data.slice(0, 6);
       console.log(data);
     });
   }
