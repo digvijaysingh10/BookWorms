098017eba87c721cf077143f6c1c3c2871f869df
m-triple-m <triplem656@gmail.com>
2021-06-07 12:20:35 +0530

    ..

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -3,12 +3,8 @@ const mongoose = require('../connection');
 const schema = mongoose.Schema({
 
     user: { type: mongoose.Types.ObjectId, ref: 'Users' },
-
     data: Object,
-
     created: Date,
-
-
 })
 
 const model = mongoose.model('orders', schema);
