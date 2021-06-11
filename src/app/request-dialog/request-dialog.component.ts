7341263bbdfb6480557b337810ae7441175d1b08
DIGVIJAY <singhdigvijay703@gmail.com>
2021-06-11 20:48:17 +0530

    error in request dialog

--- src/app/request-dialog/request-dialog.component.ts
+++ src/app/request-dialog/request-dialog.component.ts
@@ -1,7 +1,10 @@
 import { Inject, Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { app_config } from 'src/config';
+import { RequestService } from '../services/request.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-request-dialog',
@@ -10,12 +13,25 @@ import { app_config } from 'src/config';
 })
 export class RequestDialogComponent implements OnInit {
   url = app_config.api_url + '/';
+  requestList;
   constructor(
+    private requestService: RequestService,
+    private userService: UserService,
+    private router: Router,
     public dialogRef: MatDialogRef<RequestDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public requestData
   ) {}
 
   ngOnInit(): void {
     console.log(this.requestData);
+  }
+  repondToUser(user_id) {
+    this.userService
+      .addContact(this.userService.currentUser._id, user_id)
+      .subscribe((res) => {
+        console.log(res);
+        this.userService.refreshUser();
+        this.router.navigate(['/user/chat']);
+      });
   }
 }
